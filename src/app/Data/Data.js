db81558e9b516a622f58ea8506a890e6d9a8b7c1
Anish M V <anishmv@kofukuidealabs.com>
2023-12-12 18:20:19 +0530

    folder structure and dummy data

---
+++ src/app/Data/Data.js
@@ -0,0 +1,62 @@
+const data = [
+  {
+    name: "Intelligent Metal Soap",
+    price: "694.00",
+    image: "https://loremflickr.com/640/480/food",
+    id: 1,
+  },
+  {
+    name: "Elegant Fresh Cheese",
+    price: "667.00",
+    image: "https://loremflickr.com/640/480/food",
+    id: 2,
+  },
+  {
+    name: "Handcrafted Fresh Shirt",
+    price: "449.00",
+    image: "https://loremflickr.com/640/480/food",
+    id: 3,
+  },
+  {
+    name: "Awesome Frozen Hat",
+    price: "38.00",
+    image: "https://loremflickr.com/640/480/food",
+    id: 4,
+  },
+  {
+    name: "Unbranded Wooden Pants",
+    price: "998.00",
+    image: "https://loremflickr.com/640/480/food",
+    id: 5,
+  },
+  {
+    name: "Awesome Metal Chair",
+    price: "945.00",
+    image: "https://loremflickr.com/640/480/food",
+    id: 6,
+  },
+  {
+    name: "Bespoke Metal Table",
+    price: "605.00",
+    image: "https://loremflickr.com/640/480/food",
+    id: 7,
+  },
+  {
+    name: "Oriental Plastic Keyboard",
+    price: "168.00",
+    image: "https://loremflickr.com/640/480/food",
+    id: 8,
+  },
+  {
+    name: "Awesome Metal Towels",
+    price: "15.00",
+    image: "https://loremflickr.com/640/480/food",
+    id: 9,
+  },
+  {
+    name: "Luxurious Frozen Chicken",
+    price: "408.00",
+    image: "https://loremflickr.com/640/480/food",
+    id: 10,
+  },
+];
